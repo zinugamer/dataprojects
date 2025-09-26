400a7fa3e5f2a845c6db304d7c80939bffddc5a1
Williamsc286 <200028665+zinugamer@users.noreply.github.com>
2025-09-27 01:06:54 +0800

    Data_Cleaning_Project_Company_Layoffs
    Data_Cleaning_Project_Company_Layoffs

---
+++ Data_Project_Layoffs/Layoffs_Data_Cleaning_Project_in_MySQL.sql
@@ -0,0 +1,300 @@
+/*
+    Data Cleaning Project
+     - company layoffs 公司裁员情况分析
+     - Steps:
+       1. Remove Dupicates
+       2. Standardize the Data
+       3. Null Values or blank values
+       4. Remove Any Columns
+*/
+
+-- Create table for raw data
+DROP TABLE IF EXISTS world_layoffs.layoffs;
+
+CREATE TABLE world_layoffs.layoffs (
+    company VARCHAR(255),
+    location VARCHAR(255),
+    industry VARCHAR(255),
+    total_laid_off VARCHAR(255),
+    percentage_laid_off VARCHAR(255),
+    `date` VARCHAR(255),
+    stage VARCHAR(255),
+    country VARCHAR(255),
+    funds_raised_millions VARCHAR(255)
+);
+
+-- Import data by Data Import Wizard OR Python code (better since handling NaNs/Nulls)
+
+SELECT *
+FROM world_layoffs.layoffs
+;
+
+-- Create a staging table with same column names as raw data table
+DROP TABLE IF EXISTS world_layoffs.layoffs_staging;
+CREATE TABLE world_layoffs.layoffs_staging
+LIKE world_layoffs.layoffs
+;
+
+SELECT *
+FROM world_layoffs.layoffs_staging
+;
+
+-- Insert data by copying from layoffs raw data
+INSERT world_layoffs.layoffs_staging
+SELECT *
+FROM world_layoffs.layoffs
+;
+
+SELECT *
+FROM world_layoffs.layoffs_staging
+;
+
+-- (1) Remove Duplicates by creating CTE with Partitions (row numbers for each group)
+
+WITH duplicate_cte AS
+(
+SELECT *,
+ROW_NUMBER() OVER(
+PARTITION BY company, location, industry, total_laid_off, percentage_laid_off, `date`, stage, country, funds_raised_millions) AS row_num
+FROM world_layoffs.layoffs_staging
+)
+-- Show duplicated rows
+SELECT *
+FROM duplicate_cte
+WHERE row_num > 1
+;
+
+-- 检查当前分组方式是否的确是完全一致的记录，如果不是，用全部的column来分组更安全-保证记录完全一致
+SELECT *
+FROM world_layoffs.layoffs_staging
+WHERE company = 'Casper'
+;
+
+-- 'Copy to Clipboard - Create Statement'
+CREATE TABLE `world_layoffs`.`layoffs_staging2` (
+  `company` varchar(255) DEFAULT NULL,
+  `location` varchar(255) DEFAULT NULL,
+  `industry` varchar(255) DEFAULT NULL,
+  `total_laid_off` varchar(255) DEFAULT NULL,
+  `percentage_laid_off` varchar(255) DEFAULT NULL,
+  `date` varchar(255) DEFAULT NULL,
+  `stage` varchar(255) DEFAULT NULL,
+  `country` varchar(255) DEFAULT NULL,
+  `funds_raised_millions` varchar(255) DEFAULT NULL,
+  `row_num` INT DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+SELECT *
+FROM world_layoffs.layoffs_staging2
+;
+
+INSERT INTO world_layoffs.layoffs_staging2
+SELECT *,
+ROW_NUMBER() OVER(
+PARTITION BY company, location, industry, total_laid_off, percentage_laid_off, `date`, stage, country, funds_raised_millions) AS row_num
+FROM world_layoffs.layoffs_staging
+;
+
+SELECT *
+FROM world_layoffs.layoffs_staging2
+;
+SELECT *
+FROM world_layoffs.layoffs_staging2
+WHERE row_num > 1
+;
+
+-- 临时关闭安全模式，删除重复行之后，恢复安全模式
+SET SQL_SAFE_UPDATES = 0;
+
+DELETE FROM world_layoffs.layoffs_staging2 WHERE row_num > 1;
+
+SET SQL_SAFE_UPDATES = 1;  -- 可选：执行完后再打开安全模式
+
+SELECT *
+FROM world_layoffs.layoffs_staging2
+WHERE row_num > 1
+;
+
+-- (2) Standardizing Data: Find issues in yr data and then fixing it
+-- company col
+SELECT company, TRIM(company)
+FROM world_layoffs.layoffs_staging2;
+
+SET SQL_SAFE_UPDATES = 0;
+UPDATE world_layoffs.layoffs_staging2
+SET company = TRIM(company);
+SET SQL_SAFE_UPDATES = 1;
+
+SELECT company, TRIM(company)
+FROM world_layoffs.layoffs_staging2;
+
+-- industry col: find similar cols like Crypto, Crypto Currency, CryptoCurrency
+SELECT DISTINCT industry
+FROM world_layoffs.layoffs_staging2
+ORDER BY 1
+;
+
+SELECT DISTINCT industry
+FROM world_layoffs.layoffs_staging2
+WHERE industry LIKE '%Crypto%'
+;
+
+SET SQL_SAFE_UPDATES = 0;
+UPDATE world_layoffs.layoffs_staging2
+SET industry = 'Crypto' WHERE industry LIKE '%Crypto%';
+SET SQL_SAFE_UPDATES = 1;
+
+SELECT DISTINCT industry
+FROM world_layoffs.layoffs_staging2
+ORDER BY 1
+;
+
+-- country col: United States & United States.
+
+-- similar records for the same meaning
+SELECT DISTINCT country
+FROM world_layoffs.layoffs_staging2
+ORDER BY 1
+;
+
+SELECT DISTINCT country
+FROM world_layoffs.layoffs_staging2
+WHERE country LIKE '%United States%'
+;
+
+SET SQL_SAFE_UPDATES = 0;
+UPDATE world_layoffs.layoffs_staging2
+SET country = 'United States' WHERE country LIKE '%United States%';
+SET SQL_SAFE_UPDATES = 1;
+
+SELECT DISTINCT country
+FROM world_layoffs.layoffs_staging2
+WHERE country LIKE '%United States%'
+;
+
+-- method#2 TRIM(TRAILING '.' FROM country)
+SELECT DISTINCT country, TRIM(TRAILING '.' FROM country)
+FROM world_layoffs.layoffs_staging2
+ORDER BY 1
+;
+
+-- date col: IMPORTANT! if we wanna a time series visualizations, then need to clean the date col well
+-- Formatting the date col, to the correct data type
+SELECT 
+  `date`,
+  STR_TO_DATE(`date`, '%m/%d/%Y')
+FROM world_layoffs.layoffs_staging2
+;
+
+SET SQL_SAFE_UPDATES = 0;
+UPDATE world_layoffs.layoffs_staging2
+SET `date` = STR_TO_DATE(`date`, '%m/%d/%Y');
+SET SQL_SAFE_UPDATES = 1
+;
+
+SELECT 
+  `date`
+FROM world_layoffs.layoffs_staging2
+ORDER BY 1 -- dates ordered properly as chronological values instead of strings
+;
+-- Change the column type
+ALTER TABLE world_layoffs.layoffs_staging2
+MODIFY COLUMN `date` date
+;
+
+-- (3) Null Values or blank values
+-- total_laid_off
+SELECT *
+FROM world_layoffs.layoffs_staging2
+WHERE total_laid_off IS NULL
+AND percentage_laid_off IS NULL
+;
+
+-- industry
+SELECT *
+FROM world_layoffs.layoffs_staging2
+WHERE industry IS NULL
+OR industry = ''
+;
+
+
+SELECT *
+FROM world_layoffs.layoffs_staging2
+WHERE company = 'Airbnb'
+;
+
+SELECT *
+FROM world_layoffs.layoffs_staging2 t1
+JOIN world_layoffs.layoffs_staging2 t2
+  ON t1.company = t2.company
+WHERE t1.industry IS NULL
+AND t2.industry IS NOT NULL
+;
+
+-- 把blanks修改为NULL
+SET SQL_SAFE_UPDATES = 0;
+UPDATE world_layoffs.layoffs_staging2
+SET industry = NULL
+WHERE industry = '';
+SET SQL_SAFE_UPDATES = 1;
+
+
+-- Populate nulls 用存在记录的填补空缺
+SET SQL_SAFE_UPDATES = 0;
+UPDATE world_layoffs.layoffs_staging2 t1
+JOIN world_layoffs.layoffs_staging2 t2
+  ON t1.company = t2.company
+SET t1.industry = t2.industry
+WHERE t1.industry IS NULL
+AND t2.industry IS NOT NULL
+;
+SET SQL_SAFE_UPDATES = 1;
+
+-- 
+SELECT *
+FROM world_layoffs.layoffs_staging2 t1
+JOIN world_layoffs.layoffs_staging2 t2
+  ON t1.company = t2.company
+WHERE t1.industry IS NULL
+AND t2.industry IS NOT NULL
+;
+
+--
+SELECT *
+FROM world_layoffs.layoffs_staging2
+WHERE industry IS NULL
+OR industry = ''
+;
+
+-- 进一步看公司Bally's
+SELECT *
+FROM world_layoffs.layoffs_staging2
+WHERE company LIKE 'Bally%'
+
+-- (4) Remove Any Columns
+-- 对于total_laid_off和percentage_laid_off都没有，也没有总员工数的情况下，没办法通过计算得到这两列的值，只能删除
+SET SQL_SAFE_UPDATES = 0;
+DELETE
+FROM world_layoffs.layoffs_staging2
+WHERE total_laid_off IS NULL
+AND percentage_laid_off IS NULL
+;
+SET SQL_SAFE_UPDATES = 1;
+
+SELECT *
+FROM world_layoffs.layoffs_staging2
+WHERE total_laid_off IS NULL
+AND percentage_laid_off IS NULL
+;
+
+SELECT *
+FROM world_layoffs.layoffs_staging2
+;
+
+-- 去除一开始增加的column field列字段 'row_num'
+SET SQL_SAFE_UPDATES = 0;
+ALTER TABLE world_layoffs.layoffs_staging2
+DROP COLUMN row_num
+;
+SET SQL_SAFE_UPDATES = 1;
+
---
+++ Data_Project_Layoffs/import_layoffs_row_data.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import mysql.connector
+
+# 1. 读 CSV
+df = pd.read_csv("/Users/jincheng/Desktop/Data_Science/Data_Analytics/dataprojects/Data_Project_Layoffs/layoffs.csv")
+
+# Replace NaN with None for the entire DataFrame
+df = df.replace({pd.NA: None, pd.NaT: None, float('nan'): None})
+
+# 2. 连接 MySQL
+conn = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="sjc990809",
+    database="world_layoffs"
+)
+cursor = conn.cursor()
+
+# 3. 插入数据
+for _, row in df.iterrows():
+    print(tuple(row))  # Debugging: Print the row being inserted
+    cursor.execute("""
+        INSERT INTO layoffs (company, location, industry, total_laid_off, percentage_laid_off, date, stage, country, funds_raised_millions) 
+        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
+    """, tuple(row))
+
+conn.commit()
+cursor.close()
+conn.close()
